43c96a0bc1711a6d74859d82862a5418782d3078
GaetanoDellaVolpe <162832963+GaetanoDellaVolpe@users.noreply.github.com>
2024-06-25 17:58:16 +0200

    Gaetano Della Volpe

--- src/DataHelper.java
+++ src/DataHelper.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class DataHelper {
 
@@ -6,20 +8,46 @@ public class DataHelper {
 
     // restituisce una lista di numeri interi casuali compresi tra 0 e 10
     public static List<Integer> getRandomInteger(int size) {
+        Random rand = new Random();
+        size=20;
+        ArrayList<Integer> numeriRandom = new ArrayList<>();
 
-        return null;
+        for (int i = 0; i < size; i++) {
+            int numeroRandom = rand.nextInt(11);
+            numeriRandom.add(numeroRandom);
+        }
+        return List.of();
     }
 
     // restituisce una lista di numeri interi casuali compresi tra 0 e max
-    public static List<Integer> getRandomInteger(int size, int max) {
 
-        return null;
+    public static int getRandomInteger(int size, int max) {
+        size= 20;
+         max = 10;
+        ArrayList<Integer> interiRandom = new ArrayList<>();
+        Random rand = new Random();
+
+        for (int i = 0; i < size; i++) {
+            int numeroRandom = rand.nextInt(max + 1);
+            interiRandom.add(numeroRandom);
+        }
+        return size;
     }
+
 
     // restituisce una lista di numeri interi casuali compresi tra min e max
     public static List<Integer> getRandomInteger(int size, int min, int max) {
+        size= 20;
+         min = 1;
+         max = 10;
+        ArrayList<Integer> interiRandom = new ArrayList<>();
+        Random rand = new Random();
 
-        return null;
+        for (int i = 0; i < 10; i++) {
+            int numeriRandom = rand.nextInt(max - min + 1) + min;
+            interiRandom.add(numeriRandom);
+        }
+        return List.of();
     }
 
     // restituisce una lista di numeri casuali NON DUPLICATI compresi tra 0 e 10
